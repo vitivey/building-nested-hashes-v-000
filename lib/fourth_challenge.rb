ae2f432db9a4c7e8a181b9d5ee5d941bfe351110
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-11 13:18:24 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -16,7 +16,7 @@ def fourth_challenge
              :attitude => "hot-headed"
            }
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
