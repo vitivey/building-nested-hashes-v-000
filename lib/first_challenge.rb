338e58b11e0442fe74756ee49cbd8881461c5452
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-11 13:05:32 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,12 +2,29 @@ def first_challenge
    epic_tragedy = {
      :montagues => {
        :patriarch => {
-          :name => "Lord Montague"
+          :name => "Lord Montague",
          :age => "53"
        }
-       :matriarch =>
-       :hero =>
-       :hero_friends =>
+       :matriarch =>{
+         :name => "Lady Montague",
+         :age => "54"
+       }
+       :hero =>{
+         :name => "Romeo",
+         :age => "15",
+         :status => "Alive"
+       }
+       :hero_friends => [{
+           :name => "Benvolio",
+           :age => "17",
+           :attitude: "worried"
+          }, {
+            :name => "Mercutio",
+            :age => "18",
+            :attitude => "hot-headed"
+          }           
+         }
+       ]
      }
      :capulets =>
        :patriarch =>
